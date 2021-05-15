a0f13567fb9396a8d80b6a772d11b75fa4853484
aryan2121 <kayoukaan@gmail.com>
2021-05-16 01:08:46 +0530

    backend complete

--- app/src/main/java/com/amanaryan/quizzapp/MainActivity.java
+++ app/src/main/java/com/amanaryan/quizzapp/MainActivity.java
@@ -2,13 +2,93 @@ package com.amanaryan.quizzapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-
-    @Override
+int index;
+TextView Questiontxt,Truetxt,Falsetxt,Scoretxt;
+ArrayList indexarray=new ArrayList();
+String ans;
+int previousindex;
+int score=0;
+    int arrint=0;
+@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Questiontxt=findViewById(R.id.questiontxt);
+        Truetxt=findViewById(R.id.truetxt);
+        Falsetxt=findViewById(R.id.falsetxt);
+        Scoretxt=findViewById(R.id.scoretxt);
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        getWindow().setStatusBarColor(this.getResources().getColor(R.color.white));
+    }
+
+//int l=(Integer)R.integer.arraylength;
+        Integer[] arr=new Integer[14];
+//    ArrayList arr =new ArrayList<Integer>();
+        for (int i=0;i<arr.length;i++){
+            arr[i]=i;
+        }
+
+        Collections.shuffle(Arrays.asList(arr));
+        ChangeQuestion(arr[arrint]);
+Truetxt.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        if (ans.contains("T")){
+           score++;
+        }else{
+            Toast.makeText(MainActivity.this, "Wrong Answer", Toast.LENGTH_SHORT).show();
+        }
+if (arrint<10) {
+    ChangeQuestion(arr[arrint++]);
+}else{
+    Intent intent=new Intent(getApplicationContext(),endpage.class);
+    intent.putExtra("s",score+"");
+    startActivity(intent);}
+
+    }
+});
+Falsetxt.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        if (ans.contains("F")){
+            score++;
+        }else{
+            Toast.makeText(MainActivity.this, "Wrong Answer", Toast.LENGTH_SHORT).show();
+        }
+
+        if (arrint<10) {
+            ChangeQuestion(arr[arrint++]);
+        }else{
+            Intent intent=new Intent(getApplicationContext(),endpage.class);
+            intent.putExtra("s",score+"");
+            startActivity(intent);
+        }
+
+    }
+});
+
+}
+
+    private void ChangeQuestion(int x) {
+        Scoretxt.setText(score+"/10");
+        String[] question=getResources().getStringArray(R.array.questionsarray);
+        String[] answer=getResources().getStringArray(R.array.answerarray);
+        Questiontxt.setText(question[x].toString());
+        ans=answer[x].toString();
     }
 }
--- app/src/main/java/com/amanaryan/quizzapp/Splash.java
+++ app/src/main/java/com/amanaryan/quizzapp/Splash.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
 
 public class Splash extends AppCompatActivity {
 
@@ -13,19 +15,28 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
+        TextView start=findViewById(R.id.starttxt);
+        start.setVisibility(View.GONE);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             getWindow().setStatusBarColor(this.getResources().getColor(R.color.white));
         }
 
-        setContentView(R.layout.activity_splash);
         int time=3000;
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                start.setVisibility(View.VISIBLE);
             }
         },time);
+
+    start.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+
+        }
+    });
     }
 }
