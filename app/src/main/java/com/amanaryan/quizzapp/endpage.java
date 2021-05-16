54d113152ba8cbd84f280b790634a13d4aa9c038
aryan2121 <kayoukaan@gmail.com>
2021-05-16 22:21:54 +0530

    Demo app completed

--- app/src/main/java/com/amanaryan/quizzapp/endpage.java
+++ app/src/main/java/com/amanaryan/quizzapp/endpage.java
@@ -3,6 +3,7 @@ package com.amanaryan.quizzapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -17,6 +18,11 @@ public class endpage extends AppCompatActivity {
         TextView stxt=findViewById(R.id.sss);
         stxt.setText("Score:"+s);
         TextView r=findViewById(R.id.restart);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setStatusBarColor(this.getResources().getColor(R.color.white));
+        }
+
         r.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
